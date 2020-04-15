ffe0cf422ab1b6114037c4d75ccc9cdd6ca50598
Charlieen <charliedding@gmail.com>
2020-04-15 16:04:37 -0400

    Updates

---
+++ precache-manifest.75b9eb2a560607612c1e80ad071eca7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7ea5ae2602800bea8ba4a09c095f8a",
+    "url": "/Charlieen/word/index.html"
+  },
+  {
+    "revision": "14b0b23a665918f3778e",
+    "url": "/Charlieen/word/static/css/main.761506d3.chunk.css"
+  },
+  {
+    "revision": "85eb8ea29a8979d33ea6",
+    "url": "/Charlieen/word/static/js/2.605acde9.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/Charlieen/word/static/js/2.605acde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b0b23a665918f3778e",
+    "url": "/Charlieen/word/static/js/main.0f7b27c8.chunk.js"
+  },
+  {
+    "revision": "313f4c6b2c794c818c29",
+    "url": "/Charlieen/word/static/js/runtime-main.bffb6cf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Charlieen/word/static/media/logo.5d5d9eef.svg"
+  }
+]);
